d1d67f09dfb0ee0b290038d3b618cb333e9dfe43
Jeremy D Pavleck <jpavleck@gmail.com>
2022-06-28 19:04:15 -0500

    Create Fix-Web-Catalog-when-TLS-Enabled.ps1

---
+++ SCOM/Fix-Web-Catalog-when-TLS-Enabled.ps1
@@ -0,0 +1,19 @@
+# If you're having a problem with the SCOM web catalog, the following PowerShell should fix it for you.
+#
+
+
+$NetRegistryPath = “HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v2.0.50727”
+New-ItemProperty -Path $NetRegistryPath -Name “SchUseStrongCrypto” -Value “1” -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $NetRegistryPath -Name “SystemDefaultTlsVersions” -Value “1” -PropertyType DWORD -Force | Out-Null
+
+$NetRegistryPath = “HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319”
+New-ItemProperty -Path $NetRegistryPath -Name “SchUseStrongCrypto” -Value “1” -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $NetRegistryPath -Name “SystemDefaultTlsVersions” -Value “1” -PropertyType DWORD -Force | Out-Null
+
+$NetRegistryPath = “HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727”
+New-ItemProperty -Path $NetRegistryPath -Name “SchUseStrongCrypto” -Value “1” -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $NetRegistryPath -Name “SystemDefaultTlsVersions” -Value “1” -PropertyType DWORD -Force | Out-Null
+
+$NetRegistryPath = “HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319”
+New-ItemProperty -Path $NetRegistryPath -Name “SchUseStrongCrypto” -Value “1” -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $NetRegistryPath -Name “SystemDefaultTlsVersions” -Value “1” -PropertyType DWORD -Force | Out-Null
